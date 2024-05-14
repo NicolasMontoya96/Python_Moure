a4acabf0ee0ca41a5f6647bafb632f09771f77d2
nmontoya <NicolasSout>
2024-05-13 20:12:20 -0500

    first Get & Post

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -16,16 +16,47 @@ users_list = [User(id=1,name="Nicolas",surname= "Montoya", url="https://moure.de
             User(id=2, name="Brais", surname="Moure", url="https://nicolas.dev", age=20),
              User(id=3, name="Juan", surname="Perez", url="https://juan.dev", age=39)]
 
+{"id":5, 
+"name":"Brais", 
+"surname":"Moure", 
+"url":"https://nicolas.dev",
+"age":20}
+
+
 
 @app.get("/users")
 async def users():
     return users_list
 
+# Path
 @app.get("/user/{id}")
 async def user(id: int):
     users = filter(lambda user: user.id == id, users_list)
     try:
         return list(users)[0]
     except:
+        return search_user(id)
+
+# Query
+@app.get("/user/")
+async def user(id: int):
+    return search_user(id)
+
+
+
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error": "El usuario ya existe"}
+    else:
+        users_list.append(user)
+
+
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
         return {"error": "No se ha encontrado el usuario"}
+
 
